3325899f8eb9591c55c1ee6c56fa97b9a1202895
sonisuman <sonisuman67@gmail.com>
2022-07-20 02:27:38 -0400

    feature 3

--- JsonParsing.playground/Contents.swift
+++ JsonParsing.playground/Contents.swift
@@ -1,5 +1,24 @@
 import UIKit
 import Foundation
+
+struct Customer {
+  var firstName: String
+  var lastname: String
+  var age: Int
+}
+
+extension Customer {
+  init?(dictonary: [String: Any]) {
+    guard  let firstname = dictonary["firstname"] as? String,
+           let lastname =  dictonary["lastname"] as? String,
+           let age = dictonary["age"] as? Int
+    else {return nil}
+    
+    self.firstName = firstname
+    self.lastname = lastname
+    self.age = age
+  }
+}
 let json = """
 {
 "firstname": "soni",
@@ -10,10 +29,7 @@ let json = """
 """.data(using: .utf8)
 
 if let dataDictonary = try! JSONSerialization.jsonObject(with: json!, options: .allowFragments) as? [String: Any] {
-
-dataDictonary["firstname"]
-dataDictonary["lastname"]
-dataDictonary["age"]
-
+  let customer = Customer(dictonary: dataDictonary)
+  print(customer as Any)
 }
 
